329a88c77e3380a3613e9d4815373ba3b9eb8e35
sugandhi <sugandhi@iu.edu>
2020-11-06 23:35:59 -0600

    link included

--- dataCollection/yfHistoricals.py
+++ dataCollection/yfHistoricals.py
@@ -5,3 +5,8 @@ def getHistory(ticker, startDate, endDate):
     data = yf.download(str(ticker), start=str(startDate), end=str(endDate), group_by = 'column')
     data.to_csv(r"historicals" + str(ticker.upper()) + ".csv", index=True, header=True)
     print("Historical data operation successful!")
+
+"""
+Useful Link:
+https://github.com/GregBland/yfinance_article/blob/master/example_code.ipynb
+"""
